8ba59d3ee43d98655d614e1ec9415aae74a0e959
Xavier Gastaud <xgastaud@gmail.com>
2018-03-08 17:48:25 +0100

    javascript navbar hide/show on scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,5 +6,7 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-
 console.log('Hello World from Webpacker')
+import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
